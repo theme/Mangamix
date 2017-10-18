e2591c983b293549a98cbafede58a7eb274e0257
theme <xenotheme@gmail.com>
2017-10-18 10:51:21 +0800

    Test open image.

--- Mangamix/ViewController.m
+++ Mangamix/ViewController.m
@@ -10,6 +10,42 @@
 
 @implementation ViewController
 
+
+
+- (IBAction)chooseImage:(id)sender {
+    
+    // Get the main window for the document.
+    NSWindow* window = _imageView.window;
+    
+    // Create and configure the panel.
+    NSOpenPanel* panel = [NSOpenPanel openPanel];
+    [panel setCanChooseDirectories:NO];
+    [panel setAllowsMultipleSelection:NO];
+    [panel setMessage:@"What image do you want to open?"];
+    
+    // Display the panel attached to the document's window.
+    [panel beginSheetModalForWindow:window completionHandler:^(NSInteger result){
+        if (result == NSFileHandlingPanelOKButton) {
+            NSArray* urls = [panel URLs];
+            
+            // Use the URLs to build a list of items to import.
+            [self openImage:urls[0]];
+        }
+        
+    }];
+}
+
+- (void) openImage:(NSURL*) url{
+    NSLog(@"%@", [NSString stringWithFormat: @"openImage:(NSURL*) %@", url.absoluteString]);
+    if (!_imageView.image){
+        _imageView.image = [[NSImage alloc] initByReferencingURL:url];
+    } else {
+//        [_imageView.image release]; // not needed because of ARC
+        _imageView.image = [[NSImage alloc] initByReferencingURL:url];
+        NSLog( @"Image is already loaded." );
+    }
+}
+
 - (void)viewDidLoad {
     [super viewDidLoad];
 
